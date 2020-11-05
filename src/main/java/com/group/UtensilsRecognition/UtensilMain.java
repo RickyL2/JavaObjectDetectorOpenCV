f8e277812510aa2a162773d1e6844fb552b54f7e
heror <heror@192.168.0.175>
2020-11-05 09:28:11 -0800

    added basic implementation of tensorflow
    got tensorflow stuff working but it is a really basic implementation.
    needs a lot more work for it to be actually usable.

--- src/main/java/com/group/UtensilsRecognition/UtensilMain.java
+++ src/main/java/com/group/UtensilsRecognition/UtensilMain.java
@@ -1,9 +1,32 @@
 package com.group.UtensilsRecognition;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+
 public class UtensilMain{
 
 	private static WebcamManager webcam;
 	private static UtensilRecognitionUI frame;
+	
+	//all this needs to put in ObjectPrediction class, to try out, replace string with file location on your pc
+    private static String modelpath = "C:/Users/heror/Documents/inception_dec_2015/tensorflow_inception_graph.pb";
+    private static String labelpath = "C:/Users/heror/Documents/inception_dec_2015/imagenet_comp_graph_label_strings.txt";
+    private static byte[] graphDef;
+    private static List<String> labels;
+    private static BufferedImage pic;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,11 +34,96 @@ public class UtensilMain{
 		webcam = new WebcamManager();
 		
 		frame = new UtensilRecognitionUI(webcam.GetWebCamPanel());
-		frame.setVisible(true);
+		frame.setVisible(true);		
 		
-		
-		
-		
+		//everything below here is just for trying out the object detection tensorflow thing
+		//this setup is only temporary
+		graphDef = readAllBytesOrExit(Paths.get(modelpath));
+		labels = readAllLinesOrExit(Paths.get(labelpath));
+		pic = webcam.takePhoto();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] imageBytes = null;
+		try {
+			ImageIO.write( pic, "jpg", baos );
+			baos.flush();
+			imageBytes = baos.toByteArray();
+			baos.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        try (Tensor image = Tensor.create(imageBytes)) 
+        {
+            float[] labelProbabilities = executeInceptionGraph(graphDef, image);
+            int bestLabelIdx = maxIndex(labelProbabilities);
+            frame.UpdateResults("");
+            frame.UpdateResults(String.format(
+                            "BEST MATCH: %s (%.2f%% likely)",
+                            labels.get(bestLabelIdx), labelProbabilities[bestLabelIdx] * 100f));
+            System.out.println(
+                String.format(
+                        "BEST MATCH: %s (%.2f%% likely)",
+                        labels.get(bestLabelIdx), labelProbabilities[bestLabelIdx] * 100f));
+            
+            for (int i = 0; i < labelProbabilities.length; i++)
+            {
+            	if(labelProbabilities[i] > 0.3)
+            	{
+            		System.out.println(labels.get(i) + " " + labelProbabilities[i] * 100f + "\n");
+            	}
+            }
+        }
 	}
+	
+	private static byte[] readAllBytesOrExit(Path path) {
+        try {
+            return Files.readAllBytes(path);
+        } catch (IOException e) {
+            System.err.println("Failed to read [" + path + "]: " + e.getMessage());
+            System.exit(1);
+        }
+        return null;
+	}
+	
+	private static List<String> readAllLinesOrExit(Path path) {
+        try {
+            return Files.readAllLines(path, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            System.err.println("Failed to read [" + path + "]: " + e.getMessage());
+            System.exit(0);
+        }
+        return null;
+    }
+	
+	private static float[] executeInceptionGraph(byte[] graphDef, Tensor image)
+    {
+        Graph g = new Graph();
+        g.importGraphDef(graphDef);
+        try (Session s = new Session(g);
+                Tensor result = s.runner().feed("DecodeJpeg/contents", image).fetch("softmax").run().get(0)) 
+        {
+            final long[] rshape = result.shape();
+            if (result.numDimensions() != 2 || rshape[0] != 1)
+            {
+                throw new RuntimeException(
+                        String.format(
+                                "Expected model to produce a [1 N] shaped tensor where N is the number of labels, instead it produced one with shape %s",
+                                Arrays.toString(rshape)));
+            }
+            int nlabels = (int) rshape[1];
+            return result.copyTo(new float[1][nlabels])[0];
+        }
+    }
+	
+	private static int maxIndex(float[] probabilities) {
+        int best = 0;
+        for (int i = 1; i < probabilities.length; ++i) {
+            if (probabilities[i] > probabilities[best]) {
+                best = i;
+            }
+        }
+        return best;
+    }
 	
 }
