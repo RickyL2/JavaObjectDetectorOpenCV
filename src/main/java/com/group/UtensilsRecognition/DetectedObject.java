f50134e3aacaacc52e1ffba0254d936129503f48
heror <heror@192.168.0.175>
2020-12-01 16:57:34 -0800

    I did it, open cv works
    like a boss *drop the mic*

---
+++ src/main/java/com/group/UtensilsRecognition/DetectedObject.java
@@ -0,0 +1,46 @@
+package com.group.UtensilsRecognition;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Stroke;
+import java.awt.geom.Rectangle2D;
+
+//A class for saving properties for a detected object
+//everything pretty self-explanatory
+public class DetectedObject {
+	
+	private float strokeWidth = 0.0f;
+	private Color objectColor = null;
+	private Rectangle2D.Double boundingBox = null;
+	private String objectName = null;
+	
+	public DetectedObject(float strokeWidth, Color objectColor,
+							Rectangle2D.Double boundingBox, String objectName)
+	{
+		this.strokeWidth = strokeWidth;
+		this.objectColor = objectColor;
+		this.boundingBox = boundingBox;
+		this.objectName = objectName;
+	}
+	
+	public Stroke getStroke()
+	{
+		return new BasicStroke(strokeWidth);
+	}
+	
+	public Color getColor()
+	{
+		return objectColor;
+	}
+	
+	public Rectangle2D.Double getShape()
+	{
+		return boundingBox;
+	}
+	
+	public String getName()
+	{
+		return objectName;
+	}
+	
+}
